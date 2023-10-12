0c23d66d8dc0413fb2d508cc34697fa446638598
Sheikh Bayazid <sheikhbayazid@gmail.com>
2023-10-12 11:23:15 +0200

    Create MicrophonePermissionError

---
+++ Sources/PermissionKit/Errors/MicrophonePermissionError.swift
@@ -0,0 +1,13 @@
+//
+//  MicrophonePermissionError.swift
+//  PermissionKit
+//
+//  Created by Sheikh Bayazid on 2023-10-12.
+//
+
+import Foundation
+
+enum MicrophonePermissionError: Error {
+    case denied
+    case unknown
+}
